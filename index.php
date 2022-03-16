68739d97b2e223ca67f52265943e653b9cc8f7c8
Mehmet Can <86019030+mehmet-c4n@users.noreply.github.com>
2022-03-16 06:25:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,16 +12,16 @@ function trupper($text)
 }
 
 $sehirler=["Adana","Adıyaman","Afyon","Ağrı","Amasya","Ankara","Antalya","Artvin","Aydın","Balıkesir","Bilecik","Bingöl","Bitlis","Bolu","Burdur","Bursa","Çanakkale","Çankırı","Çorum","Denizli","Diyarbakır","Edirne","Elazığ","Erzincan","Erzurum","Eskişehir","Gaziantep","Giresun","Gümüşhane","Hakkari","Hatay","Isparta","İçel (Mersin)","İstanbul","İzmir","Kars","Kastamonu","Kayseri","Kırklareli","Kırşehir","Kocaeli","Konya","Kütahya","Malatya","Manisa","Kahramanmaraş","Mardin","Muğla","Muş","Nevşehir","Niğde","Ordu","Rize","Sakarya","Samsun","Siirt","Sinop","Sivas","Tekirdağ","Tokat","Trabzon","Tunceli","Şanlıurfa","Uşak","Van","Yozgat","Zonguldak","Aksaray","Bayburt","Karaman","Kırıkkale","Batman","Şırnak","Bartın","Ardahan","Iğdır","Yalova","Karabük","Kilis","Osmaniye","Düzce"];
-$yol_izin = 2;
-$kullanilmayan_izin = 6;
 $ceza = 0;
 $tutanak = 0;
 $plaka = 33;
-
+$yol_izin = 2;
+$kullanilmayan_izin = 6;
 $toplam_izin = ($yol_izin + $kullanilmayan_izin) - $ceza;
+
 $sevk_tarihi = "2021-10-26";
-$resmi_katilis_tarihi = "2021-10-29";
-$gercek_katilis_tarihi = "2021-10-28";
+$resmi_katilis_tarihi = "2021-12-29";
+$gercek_katilis_tarihi = "2021-12-28";
 $bugun = date('Y-m-d');
 $resmi_terhis = date('Y-m-d', strtotime($sevk_tarihi. ' + 6 months'));
 $tmi_terhis = date('Y-m-d', strtotime($resmi_terhis. ' - '.$toplam_izin.' days'));
@@ -39,7 +39,7 @@ $kalan_gun = $tarih2->diff($tarih3);
 $kalan_gun_format = intval($kalan_gun->format('%a'));
 
 $memlekete_kalan_gun = $tarih2->diff($tarih4);
-$memlekete_kalan_gun_format = intval($memlekete_kalan_gun->format('%a'));
+$memlekete_kalan_gun_format = (intval($memlekete_kalan_gun->format('%a')) - $ceza);
 
 $safak = ($kalan_gun_format-$toplam_izin);
 $atarsa = (($kalan_gun_format-$toplam_izin)-1);
@@ -57,4 +57,6 @@ echo "GEÇEN GÜN: ".$gecen_gun_format. "<br>";
 echo "ŞAFAK: ".$safak." / ".$safak_sehir."<br>";
 echo "ATARSA: ".$atarsa." / ".$atarsa_sehir."<br>";
 echo "PLAKA: ".$plaka."<br>";
-echo "MEMLEKETE KALDI: ".($memlekete_kalan_gun_format - $ceza)." GÜN / ".$memleket_tarihi;
+if($memlekete_kalan_gun_format > 0) {
+	echo "MEMLEKETE KALDI: ".($memlekete_kalan_gun_format)." GÜN / ".$memleket_tarihi;
+}
